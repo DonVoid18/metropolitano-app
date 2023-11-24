84fb0dc54156ebcba097e60b5a2f1ef15949953d
saul <saulitoloing@hotmail.com>
2023-11-23 22:32:16 -0500

    cam

--- src/pages/Ubicacion.jsx
+++ src/pages/Ubicacion.jsx
@@ -2,9 +2,9 @@ const Ubicacion = () => {
   return (
     <iframe
       className="h-screen w-full"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d982.5022779276494!2d-76.24345153047918!3d-9.933188978384125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91a7c2e1520e56e5%3A0x30dd592c220ca018!2sJir%C3%B3n%202%20de%20Mayo%20708%2C%20Hu%C3%A1nuco%2010001!5e0!3m2!1sen!2spe!4v1689968929005!5m2!1sen!2spe"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.25015186444926!2d-76.24469201386945!3d-9.933855970411654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91a7c2e6d33dfbbf%3A0x8435eec229170224!2sEsSalud%20-%20CAP%20III%20Metropolitano%20Hu%C3%A1nuco!5e0!3m2!1ses!2spe!4v1700795822699!5m2!1ses!2spe"
       allowFullScreen=""
-      loading="lazy"
+      loading="lazy"q
       referrerPolicy="no-referrer-when-downgrade"
     ></iframe>
   )
